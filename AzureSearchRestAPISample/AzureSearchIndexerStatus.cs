b74c7faffefc076480de977411902f19e09ab5df
normalian <warito_hutsu@hotmail.com>
2016-11-15 10:13:01 +0900

    first commit

---
+++ AzureSearchRestAPISample/AzureSearchIndexerStatus.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureSearchRestAPISample
+{
+    // This class was created from below REST API response
+    // https://msdn.microsoft.com/en-us/library/azure/dn946884.aspx
+    public class AzureSearchIndexerStatus
+    {
+        public string odatacontext { get; set; }
+        public string name { get; set; }
+        public string status { get; set; }
+        public Lastresult lastResult { get; set; }
+        public Executionhistory[] executionHistory { get; set; }
+    }
+
+    public class Lastresult
+    {
+        public string status { get; set; }
+        public object errorMessage { get; set; }
+        public DateTime startTime { get; set; }
+        public DateTime endTime { get; set; }
+        public object[] errors { get; set; }
+        public int itemsProcessed { get; set; }
+        public int itemsFailed { get; set; }
+        public string initialTrackingState { get; set; }
+        public string finalTrackingState { get; set; }
+    }
+
+    public class Executionhistory
+    {
+        public string status { get; set; }
+        public object errorMessage { get; set; }
+        public DateTime startTime { get; set; }
+        public DateTime endTime { get; set; }
+        public object[] errors { get; set; }
+        public int itemsProcessed { get; set; }
+        public int itemsFailed { get; set; }
+        public string initialTrackingState { get; set; }
+        public string finalTrackingState { get; set; }
+    }
+
+}
---
+++ AzureSearchRestAPISample/Program.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Azure.Search;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureSearchRestAPISample
+{
+    class Program
+    {
+        // REST API URL template of https://msdn.microsoft.com/en-us/library/azure/dn798935.aspx
+        const string INDEXER_STATUS_URL = @"https://{0}.search.windows.net/indexers/{1}/status?api-version={2}";
+
+        //Please change below variables for your environment
+        const string SERVICE_NAME = "[your azure search service name]";
+        const string ADMIN_KEY = "[your admin key]";
+        const string INDEXER_NAME = "[your indexer name]";
+        const string API_VERSION = "2015-02-28";
+
+        // Please read https://www.infoq.com/news/2016/09/HttpClient
+        readonly static HttpClient httpClient = new HttpClient();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("{0}", "Get, last indexer update status...\n");
+            GetIndexerUpdateStatus();
+            Console.ReadKey();
+        }
+
+        static async void GetIndexerUpdateStatus()
+        {
+            httpClient.DefaultRequestHeaders.Add("api-key", ADMIN_KEY);
+            var result = await httpClient.GetStringAsync(string.Format(INDEXER_STATUS_URL, SERVICE_NAME, INDEXER_NAME, API_VERSION));
+            var status = JsonConvert.DeserializeObject<AzureSearchIndexerStatus>(result);
+            Console.WriteLine("last indexer upadte of {0} was {1} at {2}.", status.name, status.lastResult.status, status.lastResult.endTime);
+        }
+    }
+}
